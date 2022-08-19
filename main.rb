aa997a0728f873df4144483cab0ce86d545402c0
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-08-19 15:45:40 +0100

    Make the script work with more subreddits

--- main.rb
+++ main.rb
@@ -2,38 +2,41 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-class DadJokesScraper
-  def initialize
+class Scraper
+  def initialize(subreddit_name)
     unparsed_json = File.read('./config.json')
     @parsed_json = JSON.parse(unparsed_json)
 
-    if @parsed_json['has_to_reset_time']
-      @parsed_json['call_date'] = Time.new
-      @parsed_json['has_to_reset_time'] = false
+    @subreddit_name = subreddit_name
+    @subreddit_url = @parsed_json[@subreddit_name]['url']
+
+    if @parsed_json[@subreddit_name]['has_to_reset_time']
+      @parsed_json[@subreddit_name]['call_date'] = Time.new
+      @parsed_json[@subreddit_name]['has_to_reset_time'] = false
     end
 
-    @total_requests = @parsed_json['total_requests']
-    @maximum_requests = @parsed_json['maximum_requests']
+    @total_requests = @parsed_json[@subreddit_name]['total_requests']
+    @maximum_requests = @parsed_json[@subreddit_name]['maximum_requests']
+    @keys = @parsed_json[@subreddit_name]['keys']
     @attempts = 0
-    @keys = @parsed_json['keys']
-    @jokes = []
+    @contents = []
   end
 
   def start
     scrape
     update_requests
-    remove_empty_or_long_jokes
-    @jokes = bind_to_keys
-    update_dad_jokes_cfg
+    parse_for_chat!
+    @contents = bind_to_keys
+    update_cfgs!
     File.write('./config.json', JSON.dump(@parsed_json))
   end
 
   private
 
   def scrape
-    while @attempts < 5 && @total_requests < @maximum_requests && @jokes.empty?
+    while @attempts < 5 && @total_requests < @maximum_requests && @contents.empty?
       begin
-        unparsed_page = URI.open('https://www.reddit.com/r/dadjokes')
+        unparsed_page = URI.open(@subreddit_url)
         parsed_page = Nokogiri::HTML(unparsed_page)
         all_posts = parsed_page.css('.rpBJOHq2PR60pnwJlUyP0 > div')
         all_posts.each_with_index do |post, index|
@@ -42,9 +45,9 @@ class DadJokesScraper
           post = post.children[0].children[0].children[2].children
           title = post[1]
           4.times { title = title.children[0] }
-          description = post[2]
-          4.times { description = description.children[0] }
-          @jokes << [title.text, description.text]
+          body = post[2]
+          4.times { body = body.children[0] }
+          @contents << [title.text, body.text]
         end
       rescue NoMethodError
         sleep 2
@@ -55,42 +58,63 @@ class DadJokesScraper
     end
   end
 
-  def bind_to_keys
-    if @jokes.flatten.length > @keys.length
-      @keys.zip(@jokes.flatten).map { |key, joke| "bind \"#{key}\" \"say #{joke}\"" }
-    else
-      @jokes.flatten.zip(@keys).map { |joke, key| "bind \"#{key}\" \"say #{joke}\"" }
+  def parse_for_chat!
+    @contents.map! do |content|
+      content.map do |content_part|
+        # content_part is either the title or the body
+        content_part = content_part.split(' ')
+        parsed_content = ['']
+        i = 0
+        j = 0
+        loop do
+          if (parsed_content[i] + " " + content_part[j]).length <= 255
+            parsed_content[i] << " " << content_part[j]
+            j += 1
+          else
+            i += 1
+            parsed_content[i] = ''
+          end
+          break if j == content_part.length
+        end
+        parsed_content
+      end
     end
   end
 
-  def remove_empty_or_long_jokes
-    # long jokes = past the game's chat's character limit
-    @jokes.filter! { |joke| joke.none? { |joke_part| joke_part.empty? || joke_part.length >= 255 } }
+  def bind_to_keys
+    if @contents.flatten.length > @keys.length
+      @keys.zip(@contents.flatten).map { |key, joke| "bind \"#{key}\" \"say #{joke}\"" }
+    else
+      @contents.flatten.zip(@keys).map { |joke, key| "bind \"#{key}\" \"say #{joke}\"" }
+    end
   end
 
   def update_requests
-    @parsed_json['total_requests'] += @attempts
+    @parsed_json[@subreddit_name]['total_requests'] += @attempts
     reset_requests if reset_requests?
   end
 
   def reset_requests?
-    Time.new - Time.parse(@parsed_json['call_date'].to_s) >= 86_400
+    # 86_400 is a day in seconds
+    Time.new - Time.parse(@parsed_json[@subreddit_name]['call_date'].to_s) >= 86_400
   end
 
   def reset_requests
-    @parsed_json['total_requests'] = 0
-    @parsed_json['has_to_reset_time'] = true
+    @parsed_json[@subreddit_name]['total_requests'] = 0
+    @parsed_json[@subreddit_name]['has_to_reset_time'] = true
   end
 
-  def update_dad_jokes_cfg
-    if @jokes.empty?
+  def update_cfgs!
+    if @contents.empty?
       puts 'No jokes were returned so try again. Your .cfg file should have the same jokes as before'
       return
     end
-    @parsed_json['dad_jokes_cfgs'].each { |cfg| File.write(cfg, (@jokes + ['host_writeconfig']).join("\n")) }
+    @parsed_json[@subreddit_name]['cfgs'].each { |cfg| File.write(cfg, (@contents + ['host_writeconfig']).join("\n")) }
     puts 'New jokes were returned, your .cfg file was updated.'
     puts 'They might be the same jokes as before, try again after a few hours if that\'s the case'
   end
 end
 
-DadJokesScraper.new.start
+# Keep the quotes
+# Name should be the same one as you set in config.json
+Scraper.new('SUBREDDIT NAME GOES HERE').start
