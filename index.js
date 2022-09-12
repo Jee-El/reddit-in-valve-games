73dd41cc59af2d3c9b519f19790f4ba469ad8da2
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-09-12 02:05:32 +0100

    feat: add a GUI

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const { ipcMain } = require('electron');
+const { execSync } = require('child_process');
+
+let clonedRepoPath = getClonedRepoPath().toString().split('\n')[1];
+let configPath = clonedRepoPath + '/config.json';
+let unparsedConfig = fs.readFileSync(configPath);
+let config = JSON.parse(unparsedConfig);
+
+function getClonedRepoPath() {
+  return execSync(
+    'bash -c "find ~ -type d -name reddit-in-valve-games 2> /dev/null"'
+  );
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.resolve(clonedRepoPath + '/preload.js'),
+    },
+  });
+  win.loadFile(clonedRepoPath + '/index.html');
+};
+
+ipcMain.on('saveSetup', (e, newConfig) => {
+  let subredditName = newConfig.subredditName;
+  config.currently_used_subreddit_name = subredditName;
+  for (let key in newConfig) {
+    if (key == 'subredditName') continue;
+    config[subredditName][key] = newConfig[key];
+  }
+  fs.writeFileSync(configPath, JSON.stringify(config));
+});
+
+ipcMain.on('addSubreddit', (e, newSubreddit) => {
+  let subredditName = newSubreddit.subredditName;
+  config[subredditName] = {
+    url: newSubreddit.link,
+    paths: newSubreddit.paths,
+    keys: newSubreddit.keys,
+    call_date: null,
+    has_to_reset_call_date: true,
+    total_requests: 0,
+    maximum_requests: 30,
+  };
+  fs.writeFileSync(configPath, JSON.stringify(config));
+});
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ preload.js
@@ -0,0 +1,158 @@
+const { ipcRenderer } = require('electron');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+let clonedRepoPath = getClonedRepoPath().toString().split('\n')[1];
+let configPath = clonedRepoPath + '/config.json';
+let unparsedConfig = fs.readFileSync(configPath);
+let config = JSON.parse(unparsedConfig);
+
+function getClonedRepoPath() {
+  return execSync(
+    'bash -c "find ~ -type d -name reddit-in-valve-games 2> /dev/null"'
+  );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const firstPathInput = document.querySelector('#first-path');
+  const secondPathInput = document.querySelector('#second-path');
+  const keysInput = document.querySelector('#keys');
+  keysInput.addEventListener('change', (e) => {
+    e.target.value = e.target.value.replace(' ', '').split(',').join(', ');
+  });
+
+  if (window.location.href.includes('index.html')) {
+    var subredditsSelect = document.querySelector('select');
+    subredditsSelect.addEventListener('input', () =>
+      updateInput(subredditsSelect, firstPathInput, secondPathInput, keysInput)
+    );
+    const saveBtn = document.querySelector('#save-btn');
+    saveBtn.addEventListener('click', () =>
+      getUpdatedConfig(
+        subredditsSelect,
+        firstPathInput,
+        secondPathInput,
+        keysInput
+      )
+    );
+    const runBtn = document.querySelector('#run-btn');
+    runBtn.addEventListener('click', () => {
+      getUpdatedConfig(
+        subredditsSelect,
+        firstPathInput,
+        secondPathInput,
+        keysInput
+      );
+      execSync(`bash -c "bundle exec ruby ${clonedRepoPath}/lib/main.rb"`);
+    });
+    fillUpTheDefaultForm(
+      subredditsSelect,
+      firstPathInput,
+      secondPathInput,
+      keysInput
+    );
+  } else {
+    var subredditName = document.querySelector('#subreddit-name');
+    var subredditLink = document.querySelector('#subreddit-link');
+    const addBtn = document.querySelector('#add-btn');
+    addBtn.addEventListener('click', () => {
+      let subreddit = {
+        subredditName: subredditName,
+        subredditLink: subredditLink,
+        paths: [firstPathInput, secondPathInput],
+        keys: keysInput,
+      };
+      ipcRenderer.send('addSubreddit', subreddit);
+    });
+  }
+});
+
+function fillUpTheDefaultForm(
+  subredditsSelect,
+  firstPathInput,
+  secondPathInput,
+  keysInput
+) {
+  createOptions(subredditsSelect);
+  fillUpPaths(firstPathInput, secondPathInput);
+  fillUpKeys(keysInput);
+}
+
+function createOptions(subredditsSelect) {
+  for (let key in config) {
+    if (key === 'currently_used_subreddit_name') continue;
+
+    let option = document.createElement('option');
+    option.textContent = key;
+    option.value = Object.keys(config).indexOf(key);
+    subredditsSelect.appendChild(option);
+  }
+  subredditsSelect.value = Object.keys(config).indexOf(
+    config.currently_used_subreddit_name
+  );
+}
+
+function fillUpPaths(firstPathInput, secondPathInput) {
+  let subredditName = config.currently_used_subreddit_name;
+  firstPathInput.value = config[subredditName].paths[0];
+  secondPathInput.value = config[subredditName].paths[1];
+}
+
+function fillUpKeys(keysInput) {
+  let subredditName = config.currently_used_subreddit_name;
+  keysInput.value = config[subredditName].keys.join(', ');
+}
+
+function getUpdatedConfig(
+  subredditsSelect,
+  firstPathInput,
+  secondPathInput,
+  keysInput
+) {
+  const subredditName = Array.from(document.querySelectorAll('option')).find(
+    (option) => {
+      return subredditsSelect.value === option.value;
+    }
+  ).textContent;
+  let firstPath = firstPathInput.value;
+  let secondPath = secondPathInput.value;
+  let keys = keysInput.value.replace(' ', '').split(',');
+
+  let newConfig = {
+    subredditName: subredditName,
+    paths: [firstPath, secondPath].filter((path) => path !== ''),
+    keys: keys,
+  };
+  ipcRenderer.send('saveSetup', newConfig);
+}
+
+function updateInput(
+  subredditsSelect,
+  firstPathInput,
+  secondPathInput,
+  keysInput
+) {
+  const subredditName = Array.from(document.querySelectorAll('option')).find(
+    (option) => {
+      return subredditsSelect.value === option.value;
+    }
+  ).textContent;
+  updatePaths(subredditName, firstPathInput, secondPathInput);
+  updateKeys(subredditName, keysInput);
+}
+
+function updatePaths(subredditName, firstPathInput, secondPathInput) {
+  const bothPaths = config[subredditName].paths;
+  firstPathInput.value = bothPaths[0];
+  secondPathInput.value = bothPaths[1];
+}
+
+function updateKeys(subredditName, keysInput) {
+  keysInput.value = config[subredditName].keys.join(', ');
+}
+
+function resetInput() {
+  document.querySelectorAll('input').forEach((input) => {
+    input.value = '';
+  });
+}
